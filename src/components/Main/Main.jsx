c3da9e7d5d1f4d21cb3b418c6889ad8bec80e564
Andrew Skow <anjeie24@mail.ru>
2025-06-23 15:12:18 +0300

    Заглушка при открытии страницы

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,29 +1,51 @@
 import MainColumn from "../MainColumn/MainColumn";
 import Cards from "../Cards/Cards";
 import { statusList } from "/data.js";
+import { useEffect } from "react";
+import { useState } from "react";
 
 function Main({ arrayList }) {
   // console.log(cardList);
   // console.log(statusList);
 
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2000); // 2 секунды задержки
+  }, []); // пустой массив зависимостей для для запуска только при монтировании компонента
+
+  console.log(isLoading);
+
+  if (isLoading)
+    return (
+      <>
+        <div>Страница в процессе загрузки</div>
+      </>
+    );
+
   return (
-    <main class="main">
-      <div class="container">
-        <div class="main__block">
-          <div class="main__content">
-            {statusList.map((status) => {
-              return (
-                <MainColumn title={status}>
-                  <Cards
-                    array={arrayList.filter((card) => card.status === status)}
-                  />
-                </MainColumn>
-              );
-            })}
+    <>
+      <main class="main">
+        <div class="container">
+          <div class="main__block">
+            <div class="main__content">
+              {statusList.map((status) => {
+                return (
+                  <MainColumn title={status}>
+                    <Cards
+                      array={arrayList.filter((card) => card.status === status)}
+                    />
+                  </MainColumn>
+                );
+              })}
+            </div>
           </div>
         </div>
-      </div>
-    </main>
+      </main>
+      ;
+    </>
   );
 }
 export default Main;
